e64255af5d9ad62a30a5de50745f8b838f3da84d
burakekin4 <burak2ekin@gmail.com>
2024-12-29 13:16:50 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  experimental: {
+    appDir: true as any,
+  },
+  appDir: 'src/app' as any// Assuming you want to use src/app  
+} as NextConfig
 
-export default nextConfig;
+export default nextConfig
